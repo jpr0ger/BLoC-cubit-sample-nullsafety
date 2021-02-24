a2f37cd49da14c911a78f6778b54cd6ef06c0830
jproger <jproger.dev@gmail.com>
2021-02-24 12:24:10 +0300

    Wrap widget by BlocProvider and create cubit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:bloc/bloc.dart';
+import 'package:bloc_and_cubit/cubit/weather_cubit.dart';
+import 'package:bloc_and_cubit/data/weather_repository.dart';
 import 'package:bloc_and_cubit/screens/weather_search.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,22 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: WeatherSearchScreen(),
+      home: BlocProvider(
+        create: (context) => WeatherCubit(FakeWeatherRepository()),
+        child: WeatherSearchScreen(),
+      ),
     );
-  }
-}
-
-class MyState {
-  final int field1;
-  final String field2;
-
-  MyState(this.field1, this.field2);
-}
-
-class MyCubit extends Cubit<MyState> {
-  MyCubit() : super(MyState(0, 'Initial value'));
-
-  void changeState() {
-    emit(MyState(0, 'New value'));
   }
 }
