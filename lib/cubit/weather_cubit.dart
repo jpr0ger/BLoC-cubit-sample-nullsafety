8806dde9b7dd91d16af0cd883c35d8a70b68854e
jproger <jproger.dev@gmail.com>
2021-02-24 11:16:42 +0300

    Add cubit mock

--- lib/cubit/weather_cubit.dart
+++ lib/cubit/weather_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_and_cubit/data/model/weather.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
 
 part 'weather_state.dart';
