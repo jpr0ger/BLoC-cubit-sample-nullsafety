ebe255221b3c34e7112f4841ba97d69838727dd1
jproger <jproger.dev@gmail.com>
2021-02-24 11:29:47 +0300

    Add cubit function.

--- lib/cubit/weather_cubit.dart
+++ lib/cubit/weather_cubit.dart
@@ -1,10 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bloc_and_cubit/data/model/weather.dart';
+import 'package:bloc_and_cubit/data/weather_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
 
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherInitial());
+  final WeatherRepository _weatherRepository;
+
+  WeatherCubit(this._weatherRepository) : super(WeatherInitial());
+
+  Future<void> getWeather(String cityName) async {
+    try {
+      emit(WeatherLoading());
+      final weather = await _weatherRepository.fetchWeather(cityName);
+      emit(WeatherLoaded(weather));
+    } on NetworkException {
+      emit(WeatherError('Couldn\'t load weather'));
+    }
+  }
 }
